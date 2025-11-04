3521ab533974ef94c39a0969d9648f6eea538c84
Ron Sardarian <ronaldsardarian@gmail.com>
2025-11-03 17:39:26 -0800

    Update info.yaml to reference new source file tt_um_user_module.v and add the implementation of the top-level user module with basic logic.

--- src/tt_um_user_module.v
+++ src/tt_um_user_module.v
@@ -7,7 +7,7 @@
  * - uo[0] (OUT) is an output
  */
 
-module user_module(
+module tt_um_user_module(
     input wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,  // Dedicated outputs
     input wire [7:0] uio_in,   // IOs: Input path
